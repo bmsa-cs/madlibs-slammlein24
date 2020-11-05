792bf458d872a7d0079613d2e975bac0a3300d82
Sage Lammlein <slammlein24@biomedscienceacademy.org>
2020-11-05 20:06:15 +0000

    Added a few inputs

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ MadLibs
 Author: Sage Lammlein
 Period/Core: Core 3
 """
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+object_w = input("Enter an object: ")
 
+
+print("12 years ago, " +name " was walking in " +place ". All of the sudden, a giant " +object_w" jumped out of ")
