bc4db1698cec79449ccdce42b5d10d924a5191f4
Sage Lammlein <slammlein24@biomedscienceacademy.org>
2020-11-16 13:17:25 +0000

    Final Project

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ Period/Core: Core 3
 """
 name = input("Enter a name: ")
 place = input("Enter a place: ")
-object_w = input("Enter an object: ")
+thing = input("Enter an object: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb in past tense: ")
+thing_2 = input("Enter a object: ")
+verb_2 = input("Enter a verb in past tense: ")
+number = input("Enter a number: ")
+verb_3 = input("Enter a verb in past tense: ")
+number = input("Enter a number: ")
 
-
-print("12 years ago, " +name " was walking in " +place ". All of the sudden, a giant " +object_w" jumped out of ")
+print("12 years ago, "+ name + " was walking around " + place + ". All of the sudden, a " + adjective + " bug started running towards " + name + " while holding a " + thing + ". They stood their ground as the bug " + verb + " toward them at a shocking speed. " + name + " picked up a " + thing_2 + " then " + verb_2 + " at the bug " + number + " times. The bug fell down in defeat and " + verb_3 + " away.")
